f44c98b92f21044f881bf65960054070850632ad
Xavier Engelbrecht <xengelbr@charlotte.edu>
2025-10-12 14:23:31 -0400

    feat: added main code to repo

---
+++ 1212 Grade Calculator.py
@@ -0,0 +1,117 @@
+def get_quiz_grades() -> float:
+    '''
+    Input:
+        This function does not take any input parameters.
+    Process:
+        Prompts the user for how many quizzes they have completed.
+        For each quiz, it prompts the user for their grade on the quiz.
+    Output:
+        Returns the quiz average as a float.
+    '''
+    print("----------- Quiz Grades -----------")
+    grade_sum = 0
+    num_quizzes = int(input("Enter the number for the most recent module quiz: "))
+
+    for i in range(num_quizzes):
+        grade = float(input("Enter your grade for \"Module " + str(i) + ": Quiz\" (0-100): "))
+        grade = grade / 100 # Scale the grade to be out of 1 instead of 100.
+        grade_sum += grade
+        print()
+
+    print("------------------------------------")
+
+    return grade_sum / num_quizzes # Return the average of the quiz grades.
+
+def get_problem_set_grades() -> float:
+    '''
+    Input:
+        This function does not take any input parameters.
+    Process:
+        Prompts the user for how many problem sets they have completed.
+        For each problem set, it prompts the user for their grade on the problem set.
+    Output:
+        Returns the problem set average as a float.
+    '''
+    print("-------- Problem Set Grades --------")
+    grade_sum = 0
+    num_problem_sets = int(input("Enter the number for the most recent problem set: "))
+
+    for i in range(num_problem_sets+1): # Loop through the number of problem sets, start from 0 since the first Problem Set is Problem Set 0.
+        grade = float(input("Enter your grade for \"Problem Set " + str(i) + "\" (0-1): "))
+        grade_sum += grade
+        print()
+
+    print("------------------------------------")
+
+    return grade_sum / (num_problem_sets + 1) # Return the average of the problem set grades.
+
+
+def get_checkpoint_grade() -> float:
+    '''
+    Input:
+        This function does not take any input parameters.
+    Process:
+        Prompts the user for how many problem sets they have completed.
+        For every checkpoint, prompt the user for their grade.
+        Typecast the grade to a float.
+        Calculate the average of the checkpoint grades.
+    Output:
+        Returns the average of the checkpoint grades.
+    '''
+    print("--------- Checkpoint Grade ---------")
+    num_checkpoints = int(input("Enter the number of checkpoints you have completed: "))
+    
+    total_grade = 0
+
+    for i in range(num_checkpoints):
+        grade = float(input("Enter your grade for Checkpoint " + str(i) + " (0-100): "))
+        total_grade += grade
+        print()
+
+    print("------------------------------------")
+    return total_grade/num_checkpoints # Return the average of the checkpoint grades.
+
+def main() -> None:
+    '''
+    Input:
+        This function does not take any input parameters.
+    Process:
+        Calls the functions to get the checkpoint grade, problem set average, and quiz average.
+        Calculates the coursework average.
+        Calculates the total grade estimate.
+    Output:
+        Prints the total grade estimate.
+    '''
+    print("------- 1212 Grade Estimator -------")
+    print("Made by Xavier Engelbrecht")
+    print()
+    print("Disclaimer: This is not an official tool and may not be accurate. Furthermore, this tool does not take into account any of the following:")
+    print(" - Engagement")
+    print(" - Late submissions")
+    print(" - Excused absences")
+    print(" - Improvement over time")
+    print()
+    print()
+
+    input("Press Enter to continue...") # Wait for user input before continuing.
+
+    print()
+    print("------------------------------------")
+    print()
+
+
+    # Run the functions and store the results in variables.
+    checkpoint_grade = get_checkpoint_grade()
+    problem_set_average = get_problem_set_grades()
+    quiz_average = get_quiz_grades()
+
+    coursework_average = ((problem_set_average + quiz_average) / 2) * 100
+
+    # Display the total grade estimate.
+    print("------- Total Grade Estimate -------")
+    print(f"Checkpoint Grade (60%): {round(checkpoint_grade,2)}%")
+    print(f"Coursework Average (40%): {round(coursework_average,2)}%")
+    print(f"Estimated Total Grade: {round((checkpoint_grade * 0.6) + (coursework_average * 0.4),2)}%")
+    print("------------------------------------")
+
+main()
